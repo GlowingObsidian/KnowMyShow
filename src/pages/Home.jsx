c4ff50158b967550350ae2b7907cb017b9597745
Shreyan Dey <deyshreyan24@outlook.com>
2023-09-03 15:14:18 +0530

    Implement TV Maze with show search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
 function Home() {
-  return <div>Home</div>;
+  const [searchStr, setSearchStr] = useState("Search Here");
+
+  const onSearchInputChanfe = (event) => {
+    setSearchStr(event.target.value);
+  };
+
+  const onSearch = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      `https://api.tvmaze.com/search/shows?q=${searchStr}`
+    );
+    const body = await response.json();
+    console.log(body);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSearch}>
+        <input
+          type="text"
+          value={searchStr}
+          onChange={onSearchInputChanfe}
+        ></input>
+        <button type="submit">Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default Home;
